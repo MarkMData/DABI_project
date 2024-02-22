61f05d1dd055c349536de7f8524cd43a590d8649
kevroy1983 <63068319+kevroy1983@users.noreply.github.com>
2024-02-22 19:19:18 +0000

    investigate duplications are valid transactions
    everyone appears to receive one of the same offers at least twice

--- Investigate_duplications_transcript.R
+++ Investigate_duplications_transcript.R
@@ -21,3 +21,17 @@ transcript %>% filter(person_id==duplicates[4,1])
 transcript %>% filter(person_id==duplicates[5,1])
 transcript %>% filter(person_id==duplicates[200,1])
 transcript %>% filter(person_id==duplicates[379,1])
+
+# investigate number of offers received 
+transcript %>% filter(person_id %in% duplicates$person_id, offer_received==1) %>%  
+  # group by offer and person_id
+  group_by(person_id, offer_id) %>% 
+  count() %>% 
+  # ungroup and group only by person_id
+  ungroup() %>% 
+  group_by(person_id) %>% 
+  # max number of same offer received by each person
+  summarise(max_n=max(n)) %>% 
+  ungroup() %>% 
+  # min of all the maximum offers receive
+  summarise(min(max_n))
