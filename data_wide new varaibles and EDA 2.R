5b3b61af8e9b7a8d8f2e2ab5daf88ead061e1e9a
kevroy1983 <63068319+kevroy1983@users.noreply.github.com>
2024-03-11 11:02:06 +0000

    update data_wide_temp.csv with income and age as factors

--- data_wide new varaibles and EDA 2.R
+++ data_wide new varaibles and EDA 2.R
@@ -374,5 +374,6 @@ summary(data_wide3$rfm_string)
 
 data_wide3$rfm_string<- as.character(data_wide3$rfm_string)
 
+data_wide3<- data_wide3 %>% select(-...1,-X)
 write.csv(data_wide3, "data_wide_temp.csv", row.names = FALSE) 
-data_wide3
+colnames(data_wide3)
