3b48694af58f0ea90578bf7d5de8bf774bd9ca76
kevroy1983 <63068319+kevroy1983@users.noreply.github.com>
2024-03-22 18:31:00 +0000

    investigate spending with individual offers

--- K-prototypes_mark_investigation.R
+++ K-prototypes_mark_investigation.R
@@ -6,6 +6,8 @@ test_df<-read.csv("kproto_test_data.csv")
 complete_df<-read.csv("kproto_complete_data.csv")
 
 complete_df$Cluster4<- as.factor(complete_df$Cluster4)
+complete_df<-complete_df %>% filter(ave_amount!=max(ave_amount))
+
 
 clust_colmap4 = c("#f7286d","#1faae0","#ffbf1f","#98FB98")
 ################################################################################
@@ -175,7 +177,6 @@ four_clust_summary <- complete_df %>%
     ave_amount_out = mean(ave_amount_out, na.rm = TRUE),
     tot_amount = mean(tot_amount, na.rm = TRUE),
     tot_off_rec = mean(tot_off_rec, na.rm = TRUE),
-    offer_completion_rate=mean(offer_completion_rate, na.rm=TRUE),
     offer_view_rate= mean(offer_view_rate, na.rm = TRUE),
     tot_off_comp = mean(tot_off_comp, na.rm = TRUE),
     tot_trans = mean(tot_trans, na.rm = TRUE),
@@ -209,7 +210,7 @@ ggplot(complete_df, aes(age_group))+
   geom_bar(aes(fill=Cluster4))
 ggplot(complete_df, aes(age))+
   geom_boxplot(aes(fill=Cluster4))
-complete_df %>% filter(Cluster4==3) %>% reframe(quantile(age, probs=c(0.05,0.25,0.50,0.75, 0.95))) 
+complete_df %>% filter(Cluster4==2) %>% reframe(quantile(age, probs=c(0.05,0.25,0.50,0.75, 0.95))) 
 complete_df$age_group<-factor(complete_df$age_group,levels= c("<=30","31-40","41-50","51-60","61-70","71+"))
 complete_df %>% group_by(Cluster4, age_group) %>% count() %>% 
   ungroup(age_group) %>% mutate(n/sum(n), cumsum(n)/sum(n))%>%
@@ -224,6 +225,7 @@ complete_df %>% filter(Cluster4==4) %>% reframe(quantile(income, probs=c(0.05,0.
 complete_df %>% group_by(Cluster4, income_bracket) %>% count() %>% 
   ungroup(income_bracket) %>% mutate(n/sum(n), cumsum(n)/sum(n))%>%
   print(n=Inf)
+
 # plot tenure
 ggplot(complete_df, aes(tenure))+
   geom_boxplot(aes(fill=Cluster4))
@@ -239,12 +241,12 @@ ggplot(complete_df, aes(ave_amount))+
 ggplot(complete_df, aes(log_ave_amount))+
   geom_boxplot(aes(fill=Cluster4))
 max(complete_df$ave_amount)
-complete_df %>% filter(Cluster4==4) %>% reframe(quantile(ave_amount, probs=c(0.05,0.25,0.50,0.75, 0.95))) 
+complete_df %>% filter(Cluster4==4) %>% reframe(quantile(ave_amount, probs=c(0,0.05,0.25,0.50,0.75, 0.95,1))) 
 
 # transactions
 ggplot(complete_df, aes(tot_trans))+
   geom_boxplot(aes(fill=Cluster4))
-complete_df %>% filter(Cluster4==4) %>% reframe(quantile(tot_trans, probs=c(0.05,0.25,0.50,0.75, 0.95))) 
+complete_df %>% filter(Cluster4==1) %>% reframe(quantile(tot_trans, probs=c(0,0.05,0.25,0.50,0.75, 0.95,1))) 
 
 
 ######################################
@@ -427,7 +429,7 @@ grid.arrange(p1,p2,p3,p4)
 #######################################################
 # how each cluster reacts with viewing and completing bogo and discount offers
 complete_df %>% group_by(Cluster4) %>% 
-  summarise(bogo_comp=mean(bogo_response_rate, na.rm = TRUE), bogo_view=mean(bogo_view_rate, na.rm=TRUE), disc_comp=mean(disc_response_rate, na.rm=TRUE),disc_view=mean(disc_view_rate, na.rm=TRUE))
+  summarise(bogo_comp=mean(bogo_response_rate, na.rm = TRUE), bogo_view=mean(bogo_view_rate, na.rm=TRUE), disc_comp=mean(disc_response_rate, na.rm=TRUE),disc_view=mean(disc_view_rate, na.rm=TRUE), info_view=mean(info_view_rate, na.rm=TRUE))
 
 # group by cluster and offer type to see how transactions and amount vary with offers
 period1<-complete_df %>% group_by(Cluster4, offer_type1) %>% summarise(mean(num_trans1), mean(tot_amount1), mean(ave_amount1),n(), sqrt(var(tot_amount1)),sqrt(var(ave_amount1)))
@@ -436,6 +438,7 @@ period3<-complete_df %>% group_by(Cluster4, offer_type3) %>% summarise(mean(num_
 period4<-complete_df %>% group_by(Cluster4, offer_type4) %>% summarise(mean(num_trans4), mean(tot_amount4), mean(ave_amount4),n(),sqrt(var(tot_amount4)), sqrt(var(ave_amount4))) 
 period5<-complete_df %>% group_by(Cluster4, offer_type5) %>% summarise(mean(num_trans5), mean(tot_amount5), mean(ave_amount5),n(),sqrt(var(tot_amount5)), sqrt(var(ave_amount5)))
 period6<-complete_df %>% group_by(Cluster4, offer_type6) %>% summarise(mean(num_trans6), mean(tot_amount6), mean(ave_amount6),n(),sqrt(var(tot_amount6)), sqrt(var(ave_amount6)))
+
 
 
 # group to see how they vary throughout all transactions
@@ -447,7 +450,7 @@ total_offer_behaviour %>% group_by(cluster) %>% mutate(perc_trans=transactions/s
 
 ####################################################
 ##### check how cluster 1 spends####################
-big_spenders<-comp_transcript %>% filter(Cluster4==1)
+big_spenders<-comp_transcript %>% filter(Cluster4==3)
 
 ggplot(big_spenders, aes(amount))+
   geom_histogram()
@@ -471,30 +474,308 @@ big_spenders %>% filter(person_id==unique(big_spenders$person_id)[17], amount>0)
 big_spenders %>% filter(person_id==unique(big_spenders$person_id)[18], amount>0) %>% select(amount)
 
 big_spenders %>% filter(amount>200) %>% group_by(person_id) %>% count() %>% arrange(desc(n))
+big_spenders %>% filter(amount<200, amount>100) %>% group_by(person_id) %>% count() %>% arrange(desc(n))
 big_spenders %>% filter(amount<100, amount>0) %>% group_by(person_id) %>% summarise(mean_amount=mean(amount), med_amount=median(amount)) %>% ungroup() %>% summarise(mean(mean_amount), mean(med_amount))
 big_spenders %>% filter(amount<100, amount>0) %>% summarise(mean(amount), median(amount))
 big_spenders %>% filter(amount<100, amount>0) %>% ggplot(aes(amount))+geom_histogram()
 
 big_spenders %>% filter(amount<100, amount>0) %>% reframe(quantile(amount, probs=c(0.25,0.75)))
-big_spenders %>% filter(amount>200) %>% reframe(quantile(amount, probs=c(0.25,0.75)))
+big_spenders %>% filter(amount>200) %>% reframe(quantile(amount, probs=c(0,0.05,0.25,0.75, 0.95,1)))
 max(big_spenders$amount)
 
 ################ cluster 2 ##################
-affluent_women<-comp_transcript %>% filter(Cluster4==2)
+affluent_women<-comp_transcript %>% filter(Cluster4==4)
 affluent_women %>% filter(amount>200) %>% group_by(person_id) %>% count() %>% arrange(desc(n))
 affluent_women %>% filter(amount>200) %>% select(amount) %>% arrange(desc(amount))
-affluent_women %>% filter(amount<100,amount>200) %>% select(amount) %>% arrange(desc(amount))
+affluent_women %>% filter(amount>100,amount<200) %>% select(amount) %>% arrange(desc(amount))
 max(affluent_women$amount)
 ##################cluster 4#####################
-regular_males<-comp_transcript %>% filter(Cluster4==4)
+regular_males<-comp_transcript %>% filter(Cluster4==2)
 regular_males %>% filter(amount>200) %>% group_by(person_id) %>% count() %>% arrange(desc(n))
 regular_males %>% filter(amount>200) %>% select(amount) %>% arrange(desc(amount))
-regular_males %>% filter(amount<100,amount>200) %>% select(amount) %>% arrange(desc(amount))
+regular_males %>% filter(amount>100,amount<200) %>% select(amount) %>% arrange(desc(amount))
 max(regular_males$amount)
 ###################cluster 3####################
-newbie_males<-comp_transcript %>% filter(Cluster4==3)
+newbie_males<-comp_transcript %>% filter(Cluster4==1)
 newbie_males %>% filter(amount>200) %>% group_by(person_id) %>% count() %>% arrange(desc(n))
 newbie_males %>% filter(amount>200) %>% select(amount) %>% arrange(desc(amount))
-newbie_males %>% filter(amount<100,amount>200) %>% select(amount) %>% arrange(desc(amount))
+newbie_males %>% filter(amount>100,amount<200) %>% select(amount) %>% arrange(desc(amount))
+
+complete_df %>% filter(Cluster4==1,tot_amount>0) %>% summarise(mean(ave_amount))
+complete_df %>% filter(Cluster4==1,ave_amount>0) %>% reframe(quantile(ave_amount, probs = c(0,0.25,0.5,0.75, 1)))
 
 
+#######################
+# how do they interact with each offer
+###########################
+offer_num_df<-read.csv("offer_received_only.csv")
+
+
+complete_df<- left_join(complete_df, offer_num_df, by="person_id")
+complete_df
+
+# group by cluster and offer type to see how transactions and amount vary with offers
+period1<-complete_df %>% group_by(Cluster4, offer_num1) %>% summarise(mean(num_trans1), mean(tot_amount1), mean(ave_amount1),n(), sqrt(var(tot_amount1)),sqrt(var(ave_amount1)))
+period2<-complete_df %>% group_by(Cluster4, offer_num2) %>% summarise(mean(num_trans2), mean(tot_amount2), mean(ave_amount2),n(),sqrt(var(tot_amount2)), sqrt(var(ave_amount2))) 
+period3<-complete_df %>% group_by(Cluster4, offer_num3) %>% summarise(mean(num_trans3), mean(tot_amount3), mean(ave_amount3),n(),sqrt(var(tot_amount3)), sqrt(var(ave_amount3)))
+period4<-complete_df %>% group_by(Cluster4, offer_num4) %>% summarise(mean(num_trans4), mean(tot_amount4), mean(ave_amount4),n(),sqrt(var(tot_amount4)), sqrt(var(ave_amount4))) 
+period5<-complete_df %>% group_by(Cluster4, offer_num5) %>% summarise(mean(num_trans5), mean(tot_amount5), mean(ave_amount5),n(),sqrt(var(tot_amount5)), sqrt(var(ave_amount5)))
+period6<-complete_df %>% group_by(Cluster4, offer_num6) %>% summarise(mean(num_trans6), mean(tot_amount6), mean(ave_amount6),n(),sqrt(var(tot_amount6)), sqrt(var(ave_amount6)))
+
+
+
+# group to see how they vary throughout all transactions
+total_offer_behaviour<- data.frame(period1$Cluster4, period1$offer_type1, period1[,3:6]+period2[,3:6]+period3[,3:6]+period4[,3:6]+period5[,3:6]+period6[,3:6])
+
+colnames(total_offer_behaviour) <-c("cluster","offer_type","transactions","tot_amount", "ave_amount","n")
+total_offer_behaviour %>% group_by(cluster) %>% mutate(perc_trans=transactions/sum(transactions),perc_tot_amount=tot_amount/sum(tot_amount)) %>% print(n=Inf)
+
+
+
+# # change offer_number to bogo or discount
+for(i in 1:dim(complete_df)[1]){
+  if(is.na(complete_df$offer_num1[i])){
+    complete_df$offer_num1[i]<-"none"
+  }
+  else if(complete_df$offer_num1[i]=="offer1"){
+    complete_df$offer_num1[i]<-"bogo1"
+  }
+  else if(complete_df$offer_num1[i]=="offer2"){
+    complete_df$offer_num1[i]<-"bogo2"
+  }
+  else if(complete_df$offer_num1[i]=="offer3"){
+    complete_df$offer_num1[i]<-"info1"
+  }
+  else if(complete_df$offer_num1[i]=="offer4"){
+    complete_df$offer_num1[i]<-"bogo3"
+  }
+  else if(complete_df$offer_num1[i]=="offer5"){
+    complete_df$offer_num1[i]<-"discount1"
+  }
+  else if(complete_df$offer_num1[i]=="offer6"){
+    complete_df$offer_num1[i]<-"discount2"
+  }
+  else if(complete_df$offer_num1[i]=="offer7"){
+    complete_df$offer_num1[i]<-"discount3"
+  }
+  else if(complete_df$offer_num1[i]=="offer8"){
+    complete_df$offer_num1[i]<-"info2"
+  }
+  else if(complete_df$offer_num1[i]=="offer9"){
+    complete_df$offer_num1[i]<-"bogo4"
+  }
+  else if(complete_df$offer_num1[i]=="offer10"){
+    complete_df$offer_num1[i]<-"discount4"
+  }
+}
+
+# # change offer_number to bogo or discount
+for(i in 1:dim(complete_df)[1]){
+  if(is.na(complete_df$offer_num2[i])){
+    complete_df$offer_num2[i]<-"none"
+  }
+  else if(complete_df$offer_num2[i]=="offer1"){
+    complete_df$offer_num2[i]<-"bogo1"
+  }
+  else if(complete_df$offer_num2[i]=="offer2"){
+    complete_df$offer_num2[i]<-"bogo2"
+  }
+  else if(complete_df$offer_num2[i]=="offer3"){
+    complete_df$offer_num2[i]<-"info1"
+  }
+  else if(complete_df$offer_num2[i]=="offer4"){
+    complete_df$offer_num2[i]<-"bogo3"
+  }
+  else if(complete_df$offer_num2[i]=="offer5"){
+    complete_df$offer_num2[i]<-"discount1"
+  }
+  else if(complete_df$offer_num2[i]=="offer6"){
+    complete_df$offer_num2[i]<-"discount2"
+  }
+  else if(complete_df$offer_num2[i]=="offer7"){
+    complete_df$offer_num2[i]<-"discount3"
+  }
+  else if(complete_df$offer_num2[i]=="offer8"){
+    complete_df$offer_num2[i]<-"info2"
+  }
+  else if(complete_df$offer_num2[i]=="offer9"){
+    complete_df$offer_num2[i]<-"bogo4"
+  }
+  else if(complete_df$offer_num2[i]=="offer10"){
+    complete_df$offer_num2[i]<-"discount4"
+  }
+}
+
+# # change offer_number to bogo or discount
+for(i in 1:dim(complete_df)[1]){
+  if(is.na(complete_df$offer_num3[i])){
+    complete_df$offer_num3[i]<-"none"
+  }
+  else if(complete_df$offer_num3[i]=="offer1"){
+    complete_df$offer_num3[i]<-"bogo1"
+  }
+  else if(complete_df$offer_num3[i]=="offer2"){
+    complete_df$offer_num3[i]<-"bogo2"
+  }
+  else if(complete_df$offer_num3[i]=="offer3"){
+    complete_df$offer_num3[i]<-"info1"
+  }
+  else if(complete_df$offer_num3[i]=="offer4"){
+    complete_df$offer_num3[i]<-"bogo3"
+  }
+  else if(complete_df$offer_num3[i]=="offer5"){
+    complete_df$offer_num3[i]<-"discount1"
+  }
+  else if(complete_df$offer_num3[i]=="offer6"){
+    complete_df$offer_num3[i]<-"discount2"
+  }
+  else if(complete_df$offer_num3[i]=="offer7"){
+    complete_df$offer_num3[i]<-"discount3"
+  }
+  else if(complete_df$offer_num3[i]=="offer8"){
+    complete_df$offer_num3[i]<-"info2"
+  }
+  else if(complete_df$offer_num3[i]=="offer9"){
+    complete_df$offer_num3[i]<-"bogo4"
+  }
+  else if(complete_df$offer_num3[i]=="offer10"){
+    complete_df$offer_num3[i]<-"discount4"
+  }
+}
+
+# # change offer_number to bogo or discount
+for(i in 1:dim(complete_df)[1]){
+  if(is.na(complete_df$offer_num4[i])){
+    complete_df$offer_num4[i]<-"none"
+  }
+  else if(complete_df$offer_num4[i]=="offer1"){
+    complete_df$offer_num4[i]<-"bogo1"
+  }
+  else if(complete_df$offer_num4[i]=="offer2"){
+    complete_df$offer_num4[i]<-"bogo2"
+  }
+  else if(complete_df$offer_num4[i]=="offer3"){
+    complete_df$offer_num4[i]<-"info1"
+  }
+  else if(complete_df$offer_num4[i]=="offer4"){
+    complete_df$offer_num4[i]<-"bogo3"
+  }
+  else if(complete_df$offer_num4[i]=="offer5"){
+    complete_df$offer_num4[i]<-"discount1"
+  }
+  else if(complete_df$offer_num4[i]=="offer6"){
+    complete_df$offer_num4[i]<-"discount2"
+  }
+  else if(complete_df$offer_num4[i]=="offer7"){
+    complete_df$offer_num4[i]<-"discount3"
+  }
+  else if(complete_df$offer_num4[i]=="offer8"){
+    complete_df$offer_num4[i]<-"info2"
+  }
+  else if(complete_df$offer_num4[i]=="offer9"){
+    complete_df$offer_num4[i]<-"bogo4"
+  }
+  else if(complete_df$offer_num4[i]=="offer10"){
+    complete_df$offer_num4[i]<-"discount4"
+  }
+}
+
+# # change offer_number to bogo or discount
+for(i in 1:dim(complete_df)[1]){
+  if(is.na(complete_df$offer_num5[i])){
+    complete_df$offer_num5[i]<-"none"
+  }
+  else if(complete_df$offer_num5[i]=="offer1"){
+    complete_df$offer_num5[i]<-"bogo1"
+  }
+  else if(complete_df$offer_num5[i]=="offer2"){
+    complete_df$offer_num5[i]<-"bogo2"
+  }
+  else if(complete_df$offer_num5[i]=="offer3"){
+    complete_df$offer_num5[i]<-"info1"
+  }
+  else if(complete_df$offer_num5[i]=="offer4"){
+    complete_df$offer_num5[i]<-"bogo3"
+  }
+  else if(complete_df$offer_num5[i]=="offer5"){
+    complete_df$offer_num5[i]<-"discount1"
+  }
+  else if(complete_df$offer_num5[i]=="offer6"){
+    complete_df$offer_num5[i]<-"discount2"
+  }
+  else if(complete_df$offer_num5[i]=="offer7"){
+    complete_df$offer_num5[i]<-"discount3"
+  }
+  else if(complete_df$offer_num5[i]=="offer8"){
+    complete_df$offer_num5[i]<-"info2"
+  }
+  else if(complete_df$offer_num5[i]=="offer9"){
+    complete_df$offer_num5[i]<-"bogo4"
+  }
+  else if(complete_df$offer_num5[i]=="offer10"){
+    complete_df$offer_num5[i]<-"discount4"
+  }
+}
+
+# # change offer_number to bogo or discount
+for(i in 1:dim(complete_df)[1]){
+  if(is.na(complete_df$offer_num6[i])){
+    complete_df$offer_num6[i]<-"none"
+  }
+  else if(complete_df$offer_num6[i]=="offer1"){
+    complete_df$offer_num6[i]<-"bogo1"
+  }
+  else if(complete_df$offer_num6[i]=="offer2"){
+    complete_df$offer_num6[i]<-"bogo2"
+  }
+  else if(complete_df$offer_num6[i]=="offer3"){
+    complete_df$offer_num6[i]<-"info1"
+  }
+  else if(complete_df$offer_num6[i]=="offer4"){
+    complete_df$offer_num6[i]<-"bogo3"
+  }
+  else if(complete_df$offer_num6[i]=="offer5"){
+    complete_df$offer_num6[i]<-"discount1"
+  }
+  else if(complete_df$offer_num6[i]=="offer6"){
+    complete_df$offer_num6[i]<-"discount2"
+  }
+  else if(complete_df$offer_num6[i]=="offer7"){
+    complete_df$offer_num6[i]<-"discount3"
+  }
+  else if(complete_df$offer_num6[i]=="offer8"){
+    complete_df$offer_num6[i]<-"info2"
+  }
+  else if(complete_df$offer_num6[i]=="offer9"){
+    complete_df$offer_num6[i]<-"bogo4"
+  }
+  else if(complete_df$offer_num6[i]=="offer10"){
+    complete_df$offer_num6[i]<-"discount4"
+  }
+}
+colnames(complete_df)
+
+# group by cluster and offer type to see how transactions and amount vary with offers
+period1<-complete_df %>% group_by(Cluster4, offer_num1) %>% summarise(mean(num_trans1), mean(tot_amount1), mean(ave_amount1),n(), sqrt(var(tot_amount1)),sqrt(var(ave_amount1)))
+period2<-complete_df %>% group_by(Cluster4, offer_num2) %>% summarise(mean(num_trans2), mean(tot_amount2), mean(ave_amount2),n(),sqrt(var(tot_amount2)), sqrt(var(ave_amount2))) 
+period3<-complete_df %>% group_by(Cluster4, offer_num3) %>% summarise(mean(num_trans3), mean(tot_amount3), mean(ave_amount3),n(),sqrt(var(tot_amount3)), sqrt(var(ave_amount3)))
+period4<-complete_df %>% group_by(Cluster4, offer_num4) %>% summarise(mean(num_trans4), mean(tot_amount4), mean(ave_amount4),n(),sqrt(var(tot_amount4)), sqrt(var(ave_amount4))) 
+period5<-complete_df %>% group_by(Cluster4, offer_num5) %>% summarise(mean(num_trans5), mean(tot_amount5), mean(ave_amount5),n(),sqrt(var(tot_amount5)), sqrt(var(ave_amount5)))
+period6<-complete_df %>% group_by(Cluster4, offer_num6) %>% summarise(mean(num_trans6), mean(tot_amount6), mean(ave_amount6),n(),sqrt(var(tot_amount6)), sqrt(var(ave_amount6)))
+
+
+
+# group to see how they vary throughout all transactions
+total_offer_behaviour<- data.frame(period1$Cluster4, period1$offer_num1, period1[,3:6]+period2[,3:6]+period3[,3:6]+period4[,3:6]+period5[,3:6]+period6[,3:6])
+
+colnames(total_offer_behaviour) <-c("cluster","offer_num","transactions","tot_amount", "ave_amount","n")
+total_offer_behaviour %>% group_by(cluster) %>% mutate(perc_trans=transactions/sum(transactions),perc_tot_amount=tot_amount/sum(tot_amount)) %>% print(n=Inf)
+
+
+complete_df %>% group_by(offer_num1) %>% count()
+complete_df %>% group_by(offer_num2) %>% count()
+complete_df %>% group_by(offer_num6) %>% count()
+
+
+comp_transcript %>%  filter(time<168) %>% group_by(Cluster4) %>% summarise(max(amount))
+comp_transcript %>%  filter(Cluster4==1) %>% arrange(desc(amount))
